137533f64426000195e2a2f8687047659cf6fd45
Aishwarya Sharma <roli.mradul@gmail.com>
2025-06-13 19:43:04 +0530

    First changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import client from "./contentful";
 
-function App() {
+function BlogList() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    client
+      .getEntries({ content_type: "blogPost" })
+      .then((response) => {
+        setPosts(response.items);
+      })
+      .catch(console.error);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Blog Posts</h1>
+      {posts.map((post) => (
+        <div key={post.sys.id}>
+          <h2>{post.fields.title}</h2>
+          <img
+            src={post.fields.coverImage.fields.file.url}
+            alt={post.fields.title}
+            width="300"
+          />
+          <p>{post.fields.body.content[0].content[0].value}</p>
+        </div>
+      ))}
     </div>
   );
 }
 
-export default App;
+export default BlogList;
---
+++ src/contentful.js
@@ -0,0 +1,8 @@
+import { createClient } from "contentful";
+
+const client = createClient({
+  space: "szbhru7hpikh",
+  accessToken: "Ikeh7r3qGts7JcdB5TD9dvzd4B5zpyJrbL39GYmY9jM",
+});
+
+export default client;
